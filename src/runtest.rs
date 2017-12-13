efc2ffbc248c95b676bebe7f3253c4ec37b9d61a
Daichi Hirono <hirono@google.com>
2017-12-13 23:51:08 +0900

    Fix style

--- src/runtest.rs
+++ src/runtest.rs
@@ -5,7 +5,8 @@ use std::env;
 fn main() {
     env::set_var("RUST_LOG", "shell=info");
     loop {
-        cmd!("cargo test -- --test-threads=1 --nocapture").run().unwrap_or_default();
+        cmd!("cargo test -- --test-threads=1 --nocapture").run()
+            .unwrap_or_default();
         cmd!("inotifywait -e close_write -r .").run().unwrap();
     }
 }
